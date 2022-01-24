b6a215c84a4aa7b0ffecab6a7ce0d64c0c70d7e1
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-01-24 15:17:20 +0100

    Fix scope API call

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const form = document.querySelector(".cityform");
-const cityName = document.querySelector("#city");
 
 // Add city for API call
 const handleForm = (event) => {
     event.preventDefault();
 
     const newCity = event.target.city.value;
-    console.log(newCity)
+
+    // API call
+    const getData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${config.apiKey}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
 
 form.addEventListener("submit", handleForm);
-
-// API call
-const getData = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${config.apiKey}`);
